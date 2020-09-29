d79a9c3f8e5ea99a71f8eb4212151020193e76e6
royalturd <samridhh.rudra@hotmail.com>
2020-09-29 08:07:19 +0000

    fixed FOD permission
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- device.mk
+++ device.mk
@@ -267,6 +267,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
